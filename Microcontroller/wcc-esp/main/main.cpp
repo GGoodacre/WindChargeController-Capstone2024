5f978985d4aa35a1e28481394d6ef7c5f137c86d
GGoodacre <77134320+GGoodacre@users.noreply.github.com>
2024-02-15 11:48:46 -0800

    Basics working for esp

--- Microcontroller/wcc-esp/main/main.cpp
+++ Microcontroller/wcc-esp/main/main.cpp
@@ -1,9 +1,11 @@
 #include "Arduino.h"
 #include "SPImaster.hpp"
+#include "PowerElectronics.hpp"
 
 extern "C"
 {
     #include <stdio.h>
+    #include "esp_task_wdt.h"
 }
 
 extern "C"
@@ -12,11 +14,34 @@ extern "C"
 }
 
 
+
 void app_main(void)
 {
+
+
     SPImaster spiMaster;
+    PowerElectronics powerElec;
+
+    powerElec.setDUTY(30, RECTIFIER_INDEX);
+    delayMicroseconds(100);
+    powerElec.setDUTY(30, SEPIC_INDEX);
+    delayMicroseconds(100);
+    powerElec.setDUTY(30, LOAD_INDEX);
     while(1)
     {
-        spiMaster.write(R8_DC_BATTERY, CONFIG, 0x2f);
+        /*
+            powerElec.setDUTY(30, RECTIFIER_INDEX);
+            delayMicroseconds(100);
+            powerElec.setDUTY(50, SEPIC_INDEX);
+            delayMicroseconds(100);
+            powerElec.setDUTY(70, LOAD_INDEX);
+            delayMicroseconds(1000000);
+            powerElec.setDUTY(10, RECTIFIER_INDEX);
+            delayMicroseconds(100);
+            powerElec.setDUTY(80, SEPIC_INDEX);
+            delayMicroseconds(100);
+            powerElec.setDUTY(40, LOAD_INDEX);
+            delayMicroseconds(1000000);
+        */
     }
 }
