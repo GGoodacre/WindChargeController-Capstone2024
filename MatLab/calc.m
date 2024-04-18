8f1adeb934c3cedf0c656a0432cc606ad9de92a8
GGoodacre <77134320+GGoodacre@users.noreply.github.com>
2024-04-17 20:14:36 -0700

    Simulation Files init

---
+++ MatLab/calc.m
@@ -0,0 +1,10 @@
+Pm = 300; %Output Power
+rho = 1.225; %Air Density
+R = ;%Radius
+A = pi()*R^2; %Area swept
+omega = ;%angular speed of turbine
+Vw = ; %Wind speed
+lambda = (R*omega)/Vw; %Tip Speed Ratio
+beta = ; %Blade pitch angle
+
+Pm = (1/2)*Cp*rhol n
